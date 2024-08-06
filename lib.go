8ec8c6304e284002a2742778634b9f664bf504b2
Balazs Gibizer <gibi@redhat.com>
2024-08-06 18:46:26 +0200

    add new feature

--- lib.go
+++ lib.go
@@ -2,6 +2,9 @@ package example
 
 import "fmt"
 
-func Hello(msg string) {
-        fmt.Println("lib says: " + msg)
+func Hello(msg string, repeate int) {
+        for repeate > 0 {
+                fmt.Println("lib says: " + msg)
+                repeate-=1
+        }
 }
