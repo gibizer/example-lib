0dadf04813588d1f93066f9d0c108b8a338bd040
Balazs Gibizer <gibi@redhat.com>
2024-08-06 18:55:49 +0200

    add error handling

--- lib.go
+++ lib.go
@@ -3,6 +3,9 @@ package example
 import "fmt"
 
 func Hello(msg string, repeate int) {
+        if repeate < 0 {
+                panic("repate cannot be negative")
+        }
         for repeate > 0 {
                 fmt.Println("lib says: " + msg)
                 repeate-=1
